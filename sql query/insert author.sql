b4f720b0e292ef9432a9bd12f9d5d74790e6a896
KiselevK <kirill.kiselev220@gmail.com>
2017-11-23 20:12:49 +0200

    added insert author query

---
+++ sql query/insert author.sql
@@ -0,0 +1,21 @@
+﻿use MyLibrary
+insert Author values
+(N'Патрик Ротфусс',N'Patrick Rothfuss',N'https://fantlab.ru/autor10036'),
+(N'Робин Хобб',N'Robin Hobb',N'https://fantlab.ru/autor128'),
+(N'Джоанн Роулинг',N'Joanne Rowling',N'https://fantlab.ru/autor131'),
+(N'Скотт Линч',N'Scott Lynch',N'https://fantlab.ru/autor1047'),
+(N'Пол Хофман',N'Paul Hoffman',N'https://fantlab.ru/autor10127'),
+(N'Саба Тахир',N'Sabaa Tahir',N'https://fantlab.ru/autor43490'),
+(N'Джим Батчер ',N'Jim Butcher',N'https://fantlab.ru/autor957'),
+(N'Джо Аберкромби',N'Joe Abercrombie',N'https://fantlab.ru/autor2005'),
+(N'Кертис Джоблинг',N'Curtis Jobling',N'https://fantlab.ru/autor33600'),
+(N'Анджей Сапковский ',N'Andrzej Sapkowski',N'https://fantlab.ru/autor12'),
+(N'Роберт Сальваторе',N'Robert A. Salvatore',N'https://fantlab.ru/autor134'),
+(N'Стивен Кинг',N'Stephen King',N'https://fantlab.ru/autor22'),
+(N'Питер В. Бретт',N'Peter V. Brett',N'https://fantlab.ru/work178279'),
+(N'Джонатан Страуд',N'Jonathan Stroud',N'https://fantlab.ru/autor579'),
+(N'Джек Лондон',N'Jack London',N'https://fantlab.ru/autor3003'),
+(N'Жан-Луи Фетжен',N'Jean-Louis Fetjaine',N'https://fantlab.ru/work623075'),
+(N'Уилбур Смит',N'Wilbur Smith',N'https://fantlab.ru/autor8983'),
+(N'Марио Пьюзо',N'Mario Puzo',N'https://fantlab.ru/work767203'),
+(N'Сэйдж Блэквуд',N'Sage Blackwood',N'https://fantlab.ru/work642784')
