e64e4669dd397025d301537258c1d1fbdc16e8c4
KiselevK <kirill.kiselev220@gmail.com>
2017-12-08 19:11:38 +0200

    added data in group and series tables

--- sql query/insert group.sql
+++ sql query/insert group.sql
@@ -1,4 +1,12 @@
 ﻿use MyLibrary
 
 insert L_Group values
-(N'Мир Элдерлингов',N'The Realm of the Elderlings',2,N'https://fantlab.ru/work63846')
+(N'Мир Элдерлингов',N'The Realm of the Elderlings',2,N'https://fantlab.ru/work63846'),
+(N'Волшебный мир Гарри Поттера',N'The magic world of Harry Potter',3,N'https://fantlab.ru/work3650'),
+(N'Бартимеус',N'Bartimaeus',14,N'https://fantlab.ru/work23963'),
+--(N'',N'',,N''),
+--(N'',N'',,N''),
+--(N'',N'',,N''),
+--(N'',N'',,N''),
+--(N'',N'',,N''),
+--(N'',N'',,N''),
